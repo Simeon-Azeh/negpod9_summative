05ea63f90430cf2880e1f41e533e3319dcf50352
Lenineng <144006528+Lenineng@users.noreply.github.com>
2023-10-12 17:18:26 +0200

    Create Move-to-directory.sh

---
+++ shell_summative/Move-to-directory.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# check if the target directory exists or create it if it doesn't.
+target_dir="negpod_9-q1"
+if [ ! -d "$target_dir" ]; then
+mkdir "$target_dir"
+echo "created directory: $target_dir"
+fi
+# move the required files to the target directory
+mv main.sh "$target_dir"
+mv Students-list_0923.txt "$target_dir"
+mv Select-emails.sh "$target_dir"
+mv student-emails.txt "$target_dir"
+echo "Move files to $target_dir"
