af71d92b71df8d4ef20d1a618b312c721efa727e
keza <j.keza1@alustudent.com>
2023-10-14 06:16:51 -0700

    keza commit

---
+++ shell_summative/negpod_id-q1/backup-Negpod_ID.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#remote server details
+
+remote_host="2f05c1f8800b.3be8ebfc.alu-cod.online"
+remote_username="2f05c1f8800b"
+remote_password="d4a1d225d0abda9549d8"
+remote_location="/summative/0923-2023S"
+
+directory="backup_dir"
+
+
+scp -r $directory $remote_username@$remote_host:$remote_location
+echo "Backup done successfully"
